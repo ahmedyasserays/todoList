d8bc4cbe1f7888406adc3e7fa24eb5a633fa1b81
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-08-11 15:05:57 +0200

    secret key

--- todoList/settings.py
+++ todoList/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5z!*4ssryta^ar$#c^_0rsc#x3$##%%j1rrg$3&tuob6^crzyi'
+from decouple import config
+SECRET_KEY = os.environ.get('SECRET_KEY', config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
